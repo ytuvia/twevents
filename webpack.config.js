8ca5ffda60266dc6227e30b001556d5c82919e4d
Yehuda Tuvia <ytuvia@gmail.com>
2017-03-22 16:16:22 +0200

    webpack configgs

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config =  {
-  entry: './app/index.js',
+  entry: {
+    main: './app/index.js',
+    vendor: ['express','twitter','lodash','winston','s3-streamlogger'] 
+  },
   target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
+  plugins: [
+          new webpack.optimize.CommonsChunkPlugin({
+              name: 'vendor', // Specify the common bundle's name.
+              minChunks: function (module) {
+                 // this assumes your vendor imports exist in the node_modules directory
+                 return module.context && module.context.indexOf('node_modules') !== -1;
+              }
+          }),
+          //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+          new webpack.optimize.CommonsChunkPlugin({ 
+              name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+          })
+      ],
   module: {
     rules: [
       {test: /\.(js|jsx)$/, use: 'babel-loader'}
